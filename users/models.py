567b598ffc1870f2a196dc3af27dbd0ba5369585
Kostiantyn Perun <kosperun@gmail.com>
2025-03-15 21:48:56 -0400

    Add PaymentProviderUser

--- users/models.py
+++ users/models.py
@@ -49,3 +49,17 @@ class User(AbstractUser, BaseModel):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class PaymentProviderUser(BaseModel):
+    """Model to separate payment concerns from user data."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="payment_user")
+    customer_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.user.email} {self.customer_id}"
+
+    class Meta:
+        verbose_name = "Payment Provider User"
+        verbose_name_plural = "Payment Provider Users"
